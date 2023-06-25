e9849a27ce0f6776d27be1b31510bb1f297b4239
lk1422 <lk3ond@gmail.com>
2023-06-24 21:02:24 -0400

    Timing the test forward / backward passes

--- test_train.py
+++ test_train.py
@@ -1,4 +1,5 @@
 import torch
+import time
 import torch.nn as nn
 from bossman import BossMan
 import Server
@@ -24,19 +25,34 @@ async def set_up_boss(num_workers, model):
     
 
 async def test_train():
+
+    AVERAGE_FORWARD_PASS = 0
+    AVERAGE_BACKWARD_PASS = 0
     EPOCHS = 400
     boss = await set_up_boss(2, TEST_RESIDUAL_SMALL)
     boss.gather_dependencies()
     loss_fn = torch.nn.MSELoss()
     losses = []
+    print("Beginning Training")
     for e in range(EPOCHS):
         x,y = get_batch(32)
+        start = time.time()
         prediction = await boss.forward(x, {})
+        end = time.time()
+        AVERAGE_FORWARD_PASS+=(end-start)
         loss = loss_fn(prediction, y)
+        start = time.time()
         loss.backward()
+        end = time.time()
+        AVERAGE_BACKWARD_PASS+=(end-start)
         await boss.backward(prediction.grad)
         await boss.optim_step()
         losses.append(loss.item())
+        if((e+1)%20==0):
+            print(f"Training {(e+1)*100/EPOCHS}% Complete")
+    print(f"Average Forward Pass Time: {AVERAGE_FORWARD_PASS/EPOCHS}")
+    print(f"Average Backward Pass Time: {AVERAGE_BACKWARD_PASS/EPOCHS}")
+        
 
     x = [i for i in range(EPOCHS)]
     plt.plot(x,losses)
